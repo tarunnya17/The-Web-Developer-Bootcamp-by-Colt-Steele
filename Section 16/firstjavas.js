85ef42e8b3e7e82ff00b0e732675ed3be45bffe6
tarunnya17 <tarunnyamoye1705@gmail.com>
2024-07-10 19:14:28 +0600

    if else in JS

--- Section 16/firstjavas.js
+++ Section 16/firstjavas.js
@@ -1,3 +1,37 @@
 console.log("Hello from our first JS file");
 let total = 1 + 3;
 console.log("Goodbye!!!");
+
+let rating = 3;
+if (rating === 3) {
+    console.log("Math is fun!");
+}
+//conditions in JS
+let random = Math.random();
+if (random < 0.5) {
+    console.log("Number is less than 0.5");
+}
+else if (random > 0.5) {
+    console.log("Number is greater than 0.5");
+}
+else {
+    console.log("Number is 0.5");
+}
+console.log(random);
+
+const password = prompt("enter a password");
+if (password.length >= 6) {
+    console.log("Long Enough pass!");
+
+    if (password.indexOf(' ') === -1) {
+        console.log("valid pass")
+    }
+    else {
+        console.log("pass cannot have spaces")
+    }
+}
+else {
+    console.log("cannot be too short password");
+}
+
+
