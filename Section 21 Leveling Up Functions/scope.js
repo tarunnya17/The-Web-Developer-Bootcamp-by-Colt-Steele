3d38ff522d4fc8fab5f9843d356e7248db9bc8e7
tarunnya17 <tarunnyamoye1705@gmail.com>
2024-09-09 23:48:11 +0600

    for each, map, arrow functions

--- Section 21 Leveling Up Functions/scope.js
+++ Section 21 Leveling Up Functions/scope.js
@@ -7,7 +7,7 @@ function scubaDive() {
 
 scubaDive();
 
-//defining methods for an object square
+//defining methods for an object square:
 const square = {
     area: function (side) {
         return side * side;
@@ -15,4 +15,29 @@ const square = {
     perimeter(side) {
         return side * 4;
     }
+} //square.area(10)
+
+//use of the 'this' keyword:
+const hen = {
+    name: 'Helen',
+    eggCount: 0,
+    layAnEgg: function () {
+        this.eggCount = this.eggCount + 1
+        return "EGG"
+    }
 }
+
+//forEach:
+const num = [8, 9, 10, 11, 12, 13, 14, 15]
+
+num.forEach(function (el) {
+    console.log(el)
+})
+
+//map:
+const fullNames = [{ first: 'Albus', last: 'Dumbledore' }, { first: 'Harry', last: 'Potter' }, { first: 'Hermione', last: 'Granger' }, { first: 'Ron', last: 'Weasley' }, { first: 'Rubeus', last: 'Hagrid' }, { first: 'Minerva', last: 'McGonagall' }, { first: 'Severus', last: 'Snape' }];
+
+const firstNames = fullNames.map(function (first) {
+    return first.first
+})
+console.log(firstNames);
